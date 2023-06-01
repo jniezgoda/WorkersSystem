741809be8b68c937ce5101ec0a8923f98f3c3fe0
jniezgoda <julianiezgodaa@gmail.com>
2023-06-01 20:14:51 +0200

    add employee manager

---
+++ WorkersSystem/src/EmployeeManager.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class EmployeeManager {
+
+    private final HashMap<Integer,Employee> employees;
+
+    public EmployeeManager(HashMap<Integer, Employee> employees) {
+        this.employees = employees;
+    }
+
+
+    }
+
+
+
